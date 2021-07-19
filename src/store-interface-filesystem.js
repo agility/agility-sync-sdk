72e62d59082f8dce71f1703db7522f65a71ca7c3
azure_pipelines <support@agilitycms.com>
2021-07-19 12:38:59 -0400

    documented the filesystem interface/middleware

--- src/store-interface-filesystem.js
+++ src/store-interface-filesystem.js
@@ -9,13 +9,17 @@ require("dotenv").config({
 	path: `.env.${process.env.NODE_ENV}`,
 })
 
-
-const getFilePath = ({ options, itemType, languageCode, itemID }) => {
-	const fileName = `${itemID}.json`;
-
-	return path.join(options.rootPath, languageCode, itemType, fileName);
-}
-
+/**
+ * The function to handle saving/updating an item to your storage. This could be a Content Item, Page, Url Redirections, Sync State (state), or Sitemap.
+ * @param {Object} params - The parameters object
+ * @param {Object} params.options - A flexible object that can contain any properties specifically related to this interface
+ * @param {String} params.options.rootPath - The path to store/access the content as JSON 
+ * @param {Object} params.item - The object representing the Content Item, Page, Url Redirections, Sync State (state), or Sitemap that needs to be saved/updated
+ * @param {String} params.itemType - The type of item being saved/updated, expected values are `item`, `page`, `sitemap`, `nestedsitemap`, `state`, `urlredirections`
+ * @param {String} params.languageCode - The locale code associated to the item being saved/updated
+ * @param {(String|Number)} params.itemID - The ID of the item being saved/updated - this could be a string or number depending on the itemType
+ * @returns {Void}
+ */
 const saveItem = async ({ options, item, itemType, languageCode, itemID }) => {
 
 	let filePath = getFilePath({ options, itemType, languageCode, itemID });
@@ -30,7 +34,16 @@ const saveItem = async ({ options, item, itemType, languageCode, itemID }) => {
 	let json = JSON.stringify(item);
 	fs.writeFileSync(filePath, json);
 }
-
+/**
+ * The function to handle deleting an item to your storage. This could be a Content Item, Page, Url Redirections, Sync State (state), or Sitemap.
+ * @param {Object} params - The parameters object
+ * @param {Object} params.options - A flexible object that can contain any properties specifically related to this interface
+ * @param {String} params.options.rootPath - The path to store/access the content as JSON 
+ * @param {String} params.itemType - The type of item being deleted, expected values are `item`, `page`, `sitemap`, `nestedsitemap`, `state`, `urlredirections`
+ * @param {String} params.languageCode - The locale code associated to the item being saved/updated
+ * @param {(String|Number)} params.itemID - The ID of the item being deleted - this could be a string or number depending on the itemType
+ * @returns {Void}
+ */
 const deleteItem = async ({ options, itemType, languageCode, itemID }) => {
 
 	let filePath = getFilePath({ options, itemType, languageCode, itemID });
@@ -40,7 +53,18 @@ const deleteItem = async ({ options, itemType, languageCode, itemID }) => {
 	}
 
 }
-
+/**
+ * The function to handle updating and placing a Content Item into a "list" so that you can handle querying a collection of items.
+ * @param {Object} params - The parameters object
+ * @param {Object} params.options - A flexible object that can contain any properties specifically related to this interface
+ * @param {String} params.options.rootPath - The path to store/access the content as JSON 
+ * @param {Object} params.item - The object representing the Content Item
+ * @param {String} params.languageCode - The locale code associated to the item being saved/updated 
+ * @param {(String|Number)} params.itemID - The ID of the item being updated - this could be a string or number depending on the itemType
+ * @param {String} params.referenceName - The reference name of the Content List that this Content Item should be added to
+ * @param {String} params.definitionName - The Model name that the Content Item is based on
+ * @returns {Void}
+ */
 const mergeItemToList = async ({ options, item, languageCode, itemID, referenceName, definitionName }) => {
 
 	let contentList = await getItem({ options, itemType: "list", languageCode, itemID: referenceName });
@@ -75,7 +99,16 @@ const mergeItemToList = async ({ options, item, languageCode, itemID, referenceN
 
 	await saveItem({ options, item: contentList, itemType: "list", languageCode, itemID: referenceName });
 }
-
+/**
+ * The function to handle retrieving a Content Item, Page, Url Redirections, Sync State (state), or Sitemap
+ * @param {Object} params - The parameters object
+ * @param {Object} params.options - A flexible object that can contain any properties specifically related to this interface
+ * @param {String} params.options.rootPath - The path to store/access the content as JSON 
+ * @param {String} params.itemType - The type of item being accessed, expected values are `item`, `list`, `page`, `sitemap`, `nestedsitemap`, `state`, `urlredirections`
+ * @param {String} params.languageCode - The locale code associated to the item being accessed
+ * @param {(String|Number)} params.itemID - The ID of the item being accessed - this could be a string or number depending on the itemType
+ * @returns {Object}
+ */
 const getItem = async ({ options, itemType, languageCode, itemID }) => {
 	let filePath = getFilePath({ options, itemType, languageCode, itemID });
 
@@ -85,18 +118,23 @@ const getItem = async ({ options, itemType, languageCode, itemID }) => {
 	return JSON.parse(json);
 }
 
+/**
+ * The function to handle clearing the cache of synchronized data from the CMS
+ * @param {Object} params - The parameters object
+ * @param {Object} params.options - A flexible object that can contain any properties specifically related to this interface
+ * @param {String} params.options.rootPath - The path to store/access the content as JSON 
+ * @returns {Void}
+ */
 const clearItems = async ({ options }) => {
 	fs.rmdirSync(options.rootPath, { recursive: true })
 }
 
-const waitOnLock = async (lockFile) => {
 
-	while (await check(lockFile)) {
-		await sleep(100)
-	}
 
-}
-
+/**
+ * The function to handle multi-threaded Syncs that may be happening at the same time. If you need to prevent a sync from happening and let it wait until another sync has finished use this.
+ * @returns {Promise}
+ */
 const mutexLock = async () => {
 
 
@@ -137,7 +175,18 @@ const mutexLock = async () => {
 }
 
 
+//private function to get a wait on a lock file
+const waitOnLock = async (lockFile) => {
+	while (await check(lockFile)) {
+		await sleep(100)
+	}
+}
 
+//private function to get path of an item
+const getFilePath = ({ options, itemType, languageCode, itemID }) => {
+	const fileName = `${itemID}.json`;
+	return path.join(options.rootPath, languageCode, itemType, fileName);
+}
 
 
 module.exports = {
